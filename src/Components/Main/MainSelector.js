f1341f4ddee748787898c1cbd25ef9c10ae8932d
Mostafa-99 <59765480+Mostafa-99@users.noreply.github.com>
2020-04-03 17:20:05 +0200

    Auto logout bug solved

--- src/Components/Main/MainSelector.js
+++ src/Components/Main/MainSelector.js
@@ -3,10 +3,6 @@ import Main from './Main'
 import MainIndex from '../userIndex/MainIndex'
 
 class MainSelector extends Component {
-    componentDidMount(){
-        localStorage.setItem("isLoggedIn",false);
-        console.log(localStorage.getItem("isLoggedIn"));
-    }
     render() {
         return (
             <div>
--- src/Components/WebPlayer/CreatePlaylist.js
+++ src/Components/WebPlayer/CreatePlaylist.js
@@ -1,5 +1,4 @@
 import React,{Component} from 'react';
-import 'react-contexify/dist/ReactContexify.min.css';
 import './SideBar.css';
 import './createplaylist.css'
 
