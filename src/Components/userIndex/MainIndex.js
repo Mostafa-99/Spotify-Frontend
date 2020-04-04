0e1bee9dac1f7022e5e450ff565063e496cae958
youmnakhaled <youmnak1@gmail.com>
2020-04-03 18:55:58 -0700

    Naming,Request edited

--- src/Components/userIndex/MainIndex.js
+++ src/Components/userIndex/MainIndex.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import CardComponent from './cardComponent';
 import TopCover from './topCover';
-import Footer from '../Footer/Footer'
-import Navbar from '../Navigation/Navbar.js'
+import Footer from '../Footer/footer.js'
+import Navbar from '../Navigation/navbar.js'
 export class MainIndex extends Component {
     render() {
         {document.title ="Music for everyone - Spotify"}
