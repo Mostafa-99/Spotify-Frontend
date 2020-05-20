cf6af4d24a678b5ef7c70587019f05fc83b8807b
youmnakhaled <youmnak1@gmail.com>
2020-05-20 05:53:51 +0200

    Token of Notification

--- src/index.js
+++ src/index.js
@@ -3,9 +3,38 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
+//Firebase 
+import * as firebase from 'firebase';
 
 
 
+  var config = {
+  apiKey: "AIzaSyDstl21Iann4t-odVPIMFTXpI5ToD1jIC0",
+  authDomain: "totally-not-spotify.firebaseapp.com",
+  databaseURL: "https://totally-not-spotify.firebaseio.com",
+  projectId: "totally-not-spotify",
+  storageBucket: "totally-not-spotify.appspot.com",
+  messagingSenderId: "262598048193",
+  appId: "1:262598048193:web:8eb027331acfe77f625740",
+  measurementId: "G-YDVW3R60NT"
+};
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
+// Add the public key generated from the console here.
+messaging.usePublicVapidKey("BKWMGFcg3yIaZ8ONAeIORVydRfg1GFtMnKcCPV-jFyEXWAlbLv8nv9Wtsr4Gu5NsVHZTFl4yD0ZXcZpqsBvrIj8");
+messaging.requestPermission()
+  .then (function(){
+    console.log("Have permission");
+    return messaging.getToken();
+  })
+  .then(function(token){
+  console.log("token is "); 
+  console.log(token);
+  })
+.catch(function(err){
+  console.log("error occured")
+})
 
 ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('mybody'));
 
