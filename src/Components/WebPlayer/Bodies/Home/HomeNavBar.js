016aa3f5610d481c43c45d4c38d9569c12279e2b
youmnakhaled <youmnak1@gmail.com>
2020-05-22 23:42:59 +0200

    Recent Activity Commented

--- src/Components/WebPlayer/Bodies/Home/HomeNavBar.js
+++ src/Components/WebPlayer/Bodies/Home/HomeNavBar.js
@@ -117,7 +117,7 @@ class HomeNavBar extends Component {
                                 </div>
                                 )
                             }
-                             {/* {logInOrNot==="true"?(
+                              {/* {logInOrNot==="true"?(
                                 <div className="col-2"><RecentActivity/>
                                 </div> 
 
